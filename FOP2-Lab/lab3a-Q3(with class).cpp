dd203f33fb654062db7b54dbb45d32e33094fc3b
Sayzana Kibru <kibrusayzana@gmail.com>
2025-05-11 23:58:52 +0300

    Create lab3a-Q3(with class).cpp

---
+++ FOP2-Lab/lab3a-Q3(with class).cpp
@@ -0,0 +1,52 @@
+/*
+3. Write a C++ program that read low & high temperature of each day and find daily and weekly average 
+     temperature. Using .
+         a) structure
+         b) class 
+*/
+#include <iostream>
+using namespace std;
+typedef float real; // question 4
+
+class DailyTemp{
+    public:
+        double high;
+        double low;
+
+        double GetAvg(){
+            return (high+low)/2;
+        }
+};
+
+double getWeeklyAvg(DailyTemp weekTemps[]){
+    double total=0;
+    for(int i=0; i<7; i++){
+        total+=weekTemps[i].GetAvg();
+    }
+
+    return total/7;
+}
+
+int main(){
+
+   DailyTemp week[7];
+
+   ffor(int i=0; i<7; i++){
+    cout<<"Enter day " << i+1 << "'s highest and lowest temperature respectively: ";
+    cin >>week[i].high >> week[i].low;
+   }
+
+   cout<< "Daily average: \n";
+   for(int i=0; i<7; i++){
+    cout<<"High "<<week[i].high<< << " Low:" << week[i].low << " Daily avg: "<< week[i].GetAvg() << endl;
+   }
+
+   cout<<"This weeks average temperature is " <<  getWeeklyAvg(week);
+
+
+   
+
+
+
+    return 0;
+}
