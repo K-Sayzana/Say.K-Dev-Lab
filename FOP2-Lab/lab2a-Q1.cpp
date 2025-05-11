85df8da67381018527af0f4fe02ed8a62e224a1d
Sayzana Kibru <kibrusayzana@gmail.com>
2025-05-11 23:52:36 +0300

    Create lab2a-Q1.cpp

---
+++ FOP2-Lab/lab2a-Q1.cpp
@@ -0,0 +1,18 @@
+/*
+Write a C++ program to find the roots of a quadratic function(by passing coefficients as an argument) using function to where the function declaration and definitions are in separate files from the file where the function calling statement is written.
+*/
+
+#include <iostream>
+#include "root.hpp"
+
+
+
+
+
+int main(){
+
+    root(1, -5,6);
+
+
+    return 0;
+}
