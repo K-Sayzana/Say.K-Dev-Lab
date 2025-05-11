aaf016c5f08cc29f475cee6394bd4ea2e5ffc787
Sayzana Kibru <kibrusayzana@gmail.com>
2025-05-11 23:55:36 +0300

    Create lab2a-Q3.cpp

---
+++ FOP2-Lab/lab2a-Q3.cpp
@@ -0,0 +1,24 @@
+/*
+Write a function-based program to find the square of integer and rational number. Use two function of the same name that accept integer and double parameters by reference. 
+*/
+
+#include <iostream>
+using namespace std;
+
+
+int square(int &x){
+    return x*x;
+}
+
+double square(double &x){
+    return x*x;
+}
+
+int main(){
+
+
+
+    return 0;
+}
+
+   
